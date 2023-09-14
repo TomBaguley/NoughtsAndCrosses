baf8ddad1343f831dcbf9dfeb7f474f959723d5d
TomBaguley <tombaguley3@gmail.com>
2023-09-14 18:10:17 +0100

    When user inputs a row and column, the corresponding position will change to "X" and the board will print

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -4,15 +4,41 @@ import java.util.Arrays;
 import java.util.Arrays.*;
 
 public class Board {
-    private String [] row1 = {"-","-","-"};
-    private String [] row2 = {"-","-","-"};
     private String [] row3 = {"-","-","-"};
+    private String [] row2 = {"-","-","-"};
+    private String [] row1 = {"-","-","-"};
+
+    public String[] getRow1() {
+        return row1;
+    }
+
+    public void setRow1(int input, String replacement) {
+        row1[input-1] = replacement;
+    }
+
+    public String[] getRow2() {
+        return row2;
+    }
+
+    public void setRow2(String[] row2) {
+        this.row2 = row2;
+    }
+
+    public String[] getRow3() {
+        return row3;
+    }
+
+    public void setRow3(String[] row3) {
+        this.row3 = row3;
+    }
+
     @Override
     public String toString() {
-        String formattedRow1 = Arrays.toString(row1).replace(",","");
-        String formattedRow2 = Arrays.toString(row2).replace(",","").replace("[","|").replace("]","|");
+        String columnNumbers = (" 1 2 3 ");
         String formattedRow3 = Arrays.toString(row3).replace(",","");
+        String formattedRow2 = Arrays.toString(row2).replace(",","").replace("[","|").replace("]","|");
+        String formattedRow1 = Arrays.toString(row1).replace(",","");
 
-        return formattedRow1 + '\n' + formattedRow2 + '\n' + formattedRow3;
+        return formattedRow3 + '\n' + formattedRow2 + '\n' + formattedRow1 + '\n' + columnNumbers;
     }
 }
--- src/main/java/org/example/NoughtsAndCrosses.java
+++ src/main/java/org/example/NoughtsAndCrosses.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NoughtsAndCrosses {
     public static void main(String[] args) {
-      Board board1 = new Board();
+        Player player1 =  new Player();
+        Board board1 = new Board();
+        Scanner myScanner =  new Scanner(System.in);
+        System.out.println("Please enter the column number followed by the row number. e.g 1,2");
+        int userColumn = myScanner.nextInt();
+        int userRow = myScanner.nextInt();
+        if(userRow == 1){
+            board1.setRow1(userColumn,"X");
+        }
         System.out.println(board1);
     }
 }
