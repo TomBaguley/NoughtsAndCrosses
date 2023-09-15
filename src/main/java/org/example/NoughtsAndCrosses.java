d06fef15463ca1549968561c1e7cc9650818deb0
TomBaguley <tombaguley3@gmail.com>
2023-09-15 10:44:33 +0100

    Added turnounter which determines who goes firs from coin flip, at the end  of each turn counter goes up and player switches

--- src/main/java/org/example/NoughtsAndCrosses.java
+++ src/main/java/org/example/NoughtsAndCrosses.java
@@ -13,6 +13,7 @@ public class NoughtsAndCrosses {
 
         Player player1 =  new Player();
         Player player2 = new Player();
+        Player leadPlayer =  new Player();
         System.out.println("Player 1, please enter your name");
         player1.setPlayerName(myScanner.nextLine());
         System.out.println("Player 2, please enter your name");
@@ -22,12 +23,22 @@ public class NoughtsAndCrosses {
         System.out.println(player1.getPlayerName() + ", pick heads or tails to decide who goes first");
         gameLogic.setFlipWon(myScanner.nextLine());
         if(gameLogic.isFlipWon()){
-            System.out.println("");
+            System.out.println("Congratulations, " + player1.getPlayerName() + "! You go first");
+        }
+        else{
+            System.out.println("Unlucky, " + player1.getPlayerName() + ". " + player2.getPlayerName() + " goes first!");
+            turnCounter++;
         }
         Board board1 = new Board();
         gameLogic.setGameWon(board1.getRow1(), board1.getRow2(), board1.getRow3());
         System.out.println(board1);
         while(!gameLogic.gameWon) {
+            if(turnCounter % 2 == 1){
+                leadPlayer = player1;
+            }
+            else {
+                leadPlayer = player2;
+            }
             System.out.println("Please enter the column number followed by the row number. e.g 1,2");
             int userColumn = myScanner.nextInt();
             int userRow = myScanner.nextInt();
@@ -43,6 +54,7 @@ public class NoughtsAndCrosses {
                 System.out.println(board1);
                 gameLogic.setGameWon(board1.getRow1(), board1.getRow2(), board1.getRow3());
                 System.out.println(gameLogic.isGameWon());
+                turnCounter++;
             }
             else{
                 System.out.println("Not a valid move");
